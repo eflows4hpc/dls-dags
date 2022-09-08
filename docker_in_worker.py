c6a41aa07a34182ac2a5fd031b1bceb286bbc98d
petrova1 <m.petrova@fz-juelich.de>
2022-09-08 11:11:29 +0200

    Changed docker command to take input files from stagein, less arguments needed

--- docker_in_worker.py
+++ docker_in_worker.py
@@ -10,10 +10,12 @@ from decors import get_connection
 import docker_cmd as doc
 from docker_cmd import WORKER_DATA_LOCATION
 import os
+import uuid
 
 """This piplines is a test case for starting a clusterting algorithm with HeAT, running in a Docker environment.
 A test set of parameters with a HeAT example:
-{"oid": "b143bf73efd24d149bba4c081964b459", "image": "ghcr.io/helmholtz-analytics/heat:1.1.1-alpha", "stagein_args": ["demo_knn.py", "iris.h5"], "stageout_args": ["result.out"], "entrypoint": "/bin/bash", "command": "python"}
+{"oid": "b143bf73efd24d149bba4c081964b459", "image": "ghcr.io/helmholtz-analytics/heat:1.1.1-alpha", "entrypoint": "/bin/bash", "command": "python demo_knn.py iris.h5 result.out"}
+{"oid": "b143bf73efd24d149bba4c081964b459", "image":"hello-world"}
 Params:
     oid (str): oid of the data
     image (str): a docker contianer image
@@ -28,6 +30,7 @@ Params:
 default_args = {
     'owner': 'airflow',
 }
+input_fnames = []
 
 @dag(default_args=default_args, schedule_interval=None, start_date=days_ago(2), tags=['example', 'docker'])
 def docker_in_worker():
@@ -81,7 +84,7 @@ def docker_in_worker():
         return name_mappings   
    
     @task()
-    def load(files: dict, **kwargs):
+    def move_to_docker_host(files: dict, **kwargs):
         """This task copies the data to a location, 
         which will enable the following tasks an access to the data
 
@@ -92,13 +95,19 @@ def docker_in_worker():
         """
         print(f"Using {DW_CONNECTION_ID} connection")
         ssh_hook = get_connection(conn_id=DW_CONNECTION_ID)
-
+        user_dir_name = str(uuid.uuid4())
+        target_dir = os.path.join(WORKER_DATA_LOCATION, user_dir_name)
+        
         with ssh_hook.get_conn() as ssh_client:
+            
             sftp_client = ssh_client.open_sftp()
+
+            sftp_client.mkdir(target_dir, mode=0o755)
             for [truename, local] in files.items():
                 print(
-                    f"Copying {local} --> {DW_CONNECTION_ID}:{os.path.join(WORKER_DATA_LOCATION, truename)}")
-                sftp_client.put(local, os.path.join(WORKER_DATA_LOCATION, truename))
+                    f"Copying {local} --> {DW_CONNECTION_ID}:{os.path.join(target_dir, truename)}")
+                sftp_client.put(local, os.path.join(target_dir, truename))
+                input_fnames.append(truename)
                 # or separate cleanup task?
                 os.unlink(local)
 
@@ -110,15 +119,24 @@ def docker_in_worker():
         #     loaded_files.append(destination)
         # os.unlink(local_path)
 
-        # return loaded_files
+        return target_dir
 
     @task
-    def run_container(data_locations, **kwargs):
-        
+    def run_container(data_location, **kwargs):
+        """A task which runs in the docker worker and spins up a docker container with the an image and giver parameters.
+
+        Args:
+            image(str): contianer image
+            stageout_args (list): a list of files which are results from the execution
+            job_args (str): a string of further arguments which might be needed for the task execution
+            entrypoint (str): specify or overwrite the docker entrypoint
+            command(str): you can specify or override the command to be executed
+            args_to_dockerrun(str): docker options
+        """    
         params = kwargs['params']
-        stageout_fnames = params.get('stageout_args', []) 
+        # stageout_fnames = params.get('stageout_args', []) 
         
-        cmd = doc.get_dockercmd(params, WORKER_DATA_LOCATION)
+        cmd = doc.get_dockercmd(params, data_location)
         print(f"Executing docker command {cmd}")
         
         print(f"Using {DW_CONNECTION_ID} connection")
@@ -133,15 +151,15 @@ def docker_in_worker():
         context = get_current_context()
         task_calculate.execute(context)
         
-        return stageout_fnames
+        return data_location
 
     @task
-    def ls_results(output_files: list):
-        if not output_files:
+    def ls_results(output_dir):
+        if not output_dir:
             return "No output to stage out. Nothing more to do."
         hook = get_connection(conn_id=DW_CONNECTION_ID)
-        sp = " "
-        cmd = f"cd {WORKER_DATA_LOCATION}; ls -al {sp.join(output_files)}"
+        
+        cmd = f"ls -al {output_dir}"
         process = SSHOperator(
             task_id="print_results",
             ssh_hook=hook,
@@ -151,49 +169,50 @@ def docker_in_worker():
         process.execute(context)    
     
     @task()
-    def retrieve_res(fnames: list, **kwargs):
+    def retrieve_res(output_dir: str, **kwargs):
         """This task copies the data from the remote docker worker back to airflow workspace
 
         Args:
-            fnames (list): the files to be retrieved from the docker worker 
+            output_dir (str): the folder containing all the user files for the executed task, located on the docker worker 
         Returns:
             local_fpath (list): the path of the files copied back to the airflow host
         """
-        local_tmp_dir = Variable.get("working_dir", default_var='/tmp/')
+        local_tmp_dir = Variable.get("working_dir", default_var='/tmp/') #str(uuid.uuid4())
         local_fpath = []
         print(f"Using {DW_CONNECTION_ID} connection")
         ssh_hook = get_connection(conn_id=DW_CONNECTION_ID)
 
         with ssh_hook.get_conn() as ssh_client:
             sftp_client = ssh_client.open_sftp()
-            for name in fnames:
-                l = os.path.join(local_tmp_dir, name)
-                print(f"Copying {os.path.join(WORKER_DATA_LOCATION, name)} to {l}")
-                sftp_client.get(os.path.join(WORKER_DATA_LOCATION, name), l)
-                local_fpath.append(l)
+            
+            for fname in sftp_client.listdir(output_dir):
+                if fname not in input_fnames:
+                    l = os.path.join(local_tmp_dir, fname)
+                    print(f"Copying {os.path.join(output_dir, fname)} to {l}")
+                    sftp_client.get(os.path.join(output_dir, fname), l)
+                    local_fpath.append(l)
         
         return local_fpath
     
     @task()
-    def cleanup_doc_worker(files, **kwargs):
+    def cleanup_doc_worker(res_fpaths_local, data_on_worker, **kwargs):
         """This task deletes all the files from the docker worker
 
         # Args:
         #     fnames (list): the result files to be deleted on the docker worker  
+              data_on_worker (str): delete the folder with the user data from the docker worker
         """
-        params = kwargs['params']
-        stagein_fnames = params.get('stagein_args', [])
-        stageout_fnames = params.get('stageout_args', []) 
-        all_fnames = stagein_fnames + stageout_fnames
+
         print(f"Using {DW_CONNECTION_ID} connection")
         ssh_hook = get_connection(conn_id=DW_CONNECTION_ID)
 
         with ssh_hook.get_conn() as ssh_client:
             sftp_client = ssh_client.open_sftp()
-            for file in all_fnames:
-                print(
-                    f"Deleting file {DW_CONNECTION_ID}:{os.path.join(WORKER_DATA_LOCATION, file)}")
-                sftp_client.remove(os.path.join(WORKER_DATA_LOCATION, file))
+            d = os.path.join(WORKER_DATA_LOCATION, data_on_worker)
+            print(f"Deleting directory {DW_CONNECTION_ID}:{d}")
+            for f in sftp_client.listdir(d):
+                sftp_client.remove(f)
+            sftp_client.rmdir(d)
         
                 
     @task
@@ -247,11 +266,11 @@ def docker_in_worker():
     
     data = extract()
     files = transform(data)
-    data_locations = load(files)
-    output_fnames = run_container(data_locations)
-    ls_results(output_fnames)
-    res_fpaths = retrieve_res(output_fnames)
-    cleanup_doc_worker(res_fpaths)
+    data_location = move_to_docker_host(files)
+    data_on_worker = run_container(data_location)
+    ls_results(data_on_worker)
+    res_fpaths = retrieve_res(data_on_worker)
+    cleanup_doc_worker(res_fpaths, data_on_worker)
     errcode = stageout_results(res_fpaths)
     cleanup_local(errcode, res_fpaths)
 
