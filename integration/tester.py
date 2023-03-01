d66dede5d94558b6ed193d33f243a97d731435ee
jj <j.rybicki@fz-juelich.de>
2023-03-01 08:58:48 +0100

    target tester

--- integration/tester.py
+++ integration/tester.py
@@ -4,13 +4,13 @@ import re
 
 from argparse import ArgumentParser
 
-def get_config(host, port, login):
+def get_config(host, port, login, target):
 
     return {
         "force": True,
         "url": "https://raw.githubusercontent.com/eflows4hpc/dls-dags/master/integration/data.txt",
         "host": host,
-        "target": "/tmp/myfile",
+        "target": target,
         "login": login,
         "port": port,
         "key": "-----BEGIN OPENSSH PRIVATE KEY-----\nb3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAlwAAAAdzc2gtcn\nNhAAAAAwEAAQAAAIEAuq+ua51m1MHDqdSgZ2w/oJOcQrAyXMUUAtDNi7/OyyrVDMqHp3rR\nuNGCuzKhztwwQ8ATtoHtQADH/CYw8/T1EtaDPSvqZRmWHEujPTwsrgtulpRkBcaMO35lB2\nfENBMLRgA9cmmyTcduTqnbYL7UdMKoYhcw0yTgLlNK9H0vnbUAAAIA+0Tb/ftE2/0AAAAH\nc3NoLXJzYQAAAIEAuq+ua51m1MHDqdSgZ2w/oJOcQrAyXMUUAtDNi7/OyyrVDMqHp3rRuN\nGCuzKhztwwQ8ATtoHtQADH/CYw8/T1EtaDPSvqZRmWHEujPTwsrgtulpRkBcaMO35lB2fE\nNBMLRgA9cmmyTcduTqnbYL7UdMKoYhcw0yTgLlNK9H0vnbUAAAADAQABAAAAgQCvBjSNuk\nWFZKBP4gP80rUYlCulLlIZPb/UH/UFd2+mdOLHmj3yXCixkONzJDYlnbQ2YKdarZdEMTdN\nhHTS067LPzBJ0wOwt+Q2ZoVnJ//Glp1JoYX4ldEfGT0Ihmqn3wJscHwIGeWbM4AD/ZIa9H\n8BnMWq8d4x7W4aLOd11xxpYQAAAEALdgo2Ti1lmq9rmyAj8s5OjeJxwzdFUsuMPAGgD+D4\nqowTAehb7aLqkjtZyJn2HliLVODrNohW+uFsciQsaRLmAAAAQQDkycH+AJ7kHW4wcb8RLr\n/HU52EVKIcCv0bwqwnATKQIbz8fqCldhdD8O47rpBsD7Iyvp9PDjPFTiBXhBfqwGydAAAA\nQQDQ4/zEgPFngsuI+e4j1H4oSmLgAXsw1kcDNYq7a/JK7R7kGnIhPX7tI0qiFrq0h9iKip\niKOaW0KG6obaAX6g35AAAACWpqQGpqLTkwOAE=\n-----END OPENSSH PRIVATE KEY-----\n",
@@ -27,11 +27,12 @@ if __name__ == "__main__":
     parser.add_argument('--host', default='localhost')
     parser.add_argument('-p', '--port', type=int, default=2222)
     parser.add_argument('-u', '--user', default='eflows')
+    parser.add_argument('--target', default='/tmp/myfile')
 
     args = parser.parse_args()
 
 
-    conf = get_config(host=args.host, port=args.port, login=args.user)
+    conf = get_config(host=args.host, port=args.port, login=args.user, target=args.target)
     cfgstr = json.dumps(conf)
 
     res = subprocess.run(
