bb0c9cdd1452568b5595a403d3fffd78b1a4a377
jj <j.rybicki@fz-juelich.de>
2023-05-12 11:17:22 +0200

    retries with http ranges added

--- image_transfer.py
+++ image_transfer.py
@@ -6,12 +6,53 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
 from decors import get_connection, remove, setup, get_creds_from_vault_path
-from utils import http2ssh
+import requests
+from utils import file_exist
 
 default_args = {
     "owner": "airflow",
 }
 
+def http2ssh(url: str, ssh_client, remote_name: str, force=True, auth=None):
+    sftp_client = ssh_client.open_sftp()
+    size = file_exist(sftp=sftp_client, name=remote_name)
+    if size > 0:
+        print(f"File {remote_name} exists and has {size} bytes")
+        if force is not True:
+            return 0
+        print("Forcing overwrite")
+
+    dirname = os.path.dirname(remote_name)
+    ssh_client.exec_command(command=f"mkdir -p {dirname}")
+    ssh_client.exec_command(command=f"touch {remote_name}")
+
+    written = 0
+    required = 1
+    retries = 0
+    max_retries = 10
+    headers = None
+
+    with sftp_client.open(remote_name, "wb") as f:
+        f.set_pipelined(pipelined=True)
+        while (written < required and retries < max_retries):
+            with requests.get(url, stream=True, verify=False, auth=auth, headers=headers) as r:
+                if retries==0: # this is the first try
+                    required = int(r.headers.get('Content-Length', 0))
+                    print(f"File size is {required}")
+
+                for chunk in r.iter_content(chunk_size=1024 * 1000):
+                    written += len(chunk)
+                    content_to_write = memoryview(chunk)
+                    f.write(content_to_write)
+
+            print(f"Written {written} bytes. Content-lenght {required}")
+            
+            if required>0 and written<required:
+                print(f"This was {retries}/{max_retries} try. Size mismatch detected: {written} < {required}. Preparing range query")
+                headers = {'Range': f"bytes={written}-"}
+                retries+=1
+
+        return 0
 
 @dag(
     default_args=default_args,
@@ -20,7 +61,8 @@ default_args = {
     tags=["example"],
 )
 def transfer_image():
-    @task(retries=3, retry_delay=2, retry_exponential_backoff=True)
+    
+    @task #(retries=3, retry_delay=2, retry_exponential_backoff=True)
     def stream_upload(connection_id, **kwargs):
         params = kwargs["params"]
         force = params.get("force", True)
