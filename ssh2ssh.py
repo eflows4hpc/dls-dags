795b76dfe2a1fa55c40f7827e0f4886be9cc321b
jj <j.rybicki@fz-juelich.de>
2023-05-30 13:48:24 +0200

    ssh2ssh initial version

---
+++ ssh2ssh.py
@@ -0,0 +1,82 @@
+import os
+
+from airflow.decorators import dag, task
+from airflow.models.param import Param
+
+import pendulum
+
+from decors import get_connection
+from utils import copy_streams, RFSC, walk_dir
+
+
+def get_prefixed_params(prefix, params):
+    ret = {
+        key[len(prefix):]: value for key, value in params.items() if key.startswith(prefix)
+    }
+    return ret
+
+
+@dag(
+    schedule=None,
+    start_date=pendulum.today("UTC"),
+    params={
+        "source_vault_id": Param("", type="string"),
+        "source_host": Param("", type="string"),
+        "source_port": Param(type="integer", default=22),
+        "source_login": Param("", type="string"),
+        "source_path": Param("/tmp/", type="string"),
+        "target_vault_id": Param("", type="string"),
+        "target_host": Param("", type="string"),
+        "target_port": Param(type="integer", default=22),
+        "target_login": Param("", type="string"),
+        "target_path": Param("/tmp/", type="string"),
+    },
+)
+def ssh2ssh():
+    @task(multiple_outputs=True)
+    def copy(**context):
+        copied = {}
+        params = context["params"]
+        s_params = get_prefixed_params(prefix="source_", params=params)
+        t_params = get_prefixed_params(prefix="target_", params=params)
+        # still fallback to normal connections, used in integration testing
+        print('S params:', s_params)
+        print('T params:', t_params)
+        
+        if (s_con_id:=s_params.pop('vault_id'))=="":
+            s_con_id = s_params.get('connection_id', None)
+    
+        if (t_con_id:=t_params.pop('vault_id'))=="":
+            t_con_id = t_params.get('connection_id', None)
+        
+        source_ssh_hook = get_connection(conn_id=s_con_id, params=s_params)
+        target_ssh_hook = get_connection(conn_id=t_con_id, params=t_params)
+        target_client = target_ssh_hook.get_conn().open_sftp()
+
+        sftp_client = source_ssh_hook.get_conn().open_sftp()
+        sclient = RFSC(sftp_client)
+
+        mappings = list(walk_dir(client=sclient, path=s_params["path"], prefix=""))
+        for fname in mappings:
+            target_name = fname.replace(s_params["path"], t_params["path"])
+            print("Processing", fname, "-->", target_name)
+
+            di = os.path.dirname(target_name)
+            print("Making direcotry", di)
+            target_ssh_hook.get_conn().exec_command(command=f"mkdir -p {di}")
+            # sometimes mkdir takes longer and is not sync?
+            target_ssh_hook.get_conn().exec_command(command=f"touch {target_name}")
+
+            with target_client.open(target_name, "wb") as tr:
+                tr.set_pipelined(pipelined=True)
+                with sftp_client.open(fname, "rb") as sr:
+                    sr.set_pipelined(pipelined=True)
+                    copy_streams(inp=sr, outp=tr)
+            copied[fname] = target_name
+
+        return copied
+
+    copy()
+
+
+dag = ssh2ssh()
---
+++ tests/test_ssh2ssh.py
@@ -0,0 +1,98 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from airflow.models import DagBag
+from airflow.utils.state import DagRunState, TaskInstanceState
+from airflow.utils.types import DagRunType
+
+from ssh2ssh import get_prefixed_params
+from airflow import settings
+
+RUN_ID = "sshRUn"
+
+
+class TestSSH2SSH(unittest.TestCase):
+    def tearDown(self) -> None:
+        session = settings.Session()
+        res = session.execute(f"delete from dag_run where run_id = '{RUN_ID}'")
+        session.commit()
+
+    def test_prefixed(self):
+        params = {
+            "source_vault_id": "foo_id",
+            "source_host": "foo_host",
+            "source_port": 44,
+            "source_login": "foo_user",
+            "source_path": "/foo/path",
+            "target_vault_id": "bar_id",
+            "target_host": "bar_host",
+            "target_port": 33,
+            "target_login": "bar_user",
+            "target_path": "/bar/path",
+        }
+        cparams = get_prefixed_params(prefix="source_", params=params)
+        #self.assertEqual("vault_foo_id", conn_id)
+        self.assertEqual(cparams["host"], "foo_host")
+        self.assertEqual(cparams["port"], 44)
+
+        cparams = get_prefixed_params(prefix="target_", params=params)
+        #self.assertEqual("vault_bar_id", conn_id)
+        self.assertEqual(cparams["host"], "bar_host")
+        self.assertEqual(cparams["port"], 33)
+
+    @patch("decors.get_connection")
+    @patch("utils.walk_dir")
+    @patch("utils.copy_streams")
+    def test_run_cpy(self, cpy, w_dir, get_conn):
+        source_ssh = MagicMock()
+        source_client = MagicMock()
+        source_ssh.get_conn().__enter__().open_sftp = source_client
+
+        target_ssh = MagicMock()
+        target_client = MagicMock()
+        target_ssh.get_conn().__enter__().open_sftp = target_client
+
+        get_conn.side_effects = [source_ssh, target_ssh]
+
+        w_dir.return_value = [
+            "/home/foo/a.txt",
+            "/home/foo/b.txt",
+            "/home/foo/sub/foo.bar",
+        ]
+
+        dagbag = DagBag(".", include_examples=False)
+        dag = dagbag.get_dag(dag_id="ssh2ssh")
+        self.assertIsNotNone(dag)
+
+        dagrun = dag.create_dagrun(
+            state=DagRunState.RUNNING,
+            run_id=RUN_ID,
+            run_type=DagRunType.MANUAL,
+            conf={
+                "source_vault_id": "foo_id",
+                "source_host": "foo_host",
+                "source_port": 44,
+                "source_login": "foo_user",
+                "source_path": "/home/foo/",
+                "target_vault_id": "bar_id",
+                "target_host": "bar_host",
+                "target_port": 33,
+                "target_login": "bar_user",
+                "target_path": "/home/bar/",
+            },
+        )
+
+        ti = dagrun.get_task_instance(task_id="copy")
+        ti.task = dag.get_task(task_id="copy")
+
+        ti.run(ignore_all_deps=True, ignore_ti_state=True, test_mode=True)
+        self.assertEqual(ti.state, TaskInstanceState.SUCCESS)
+        ret = ti.xcom_pull(key="return_value")
+        print("Returned", ret)
+        self.assertDictEqual(
+            ret,
+            {
+                "/home/foo/a.txt": "/home/bar/a.txt",
+                "/home/foo/b.txt": "/home/bar/b.txt",
+                "/home/foo/sub/foo.bar": "/home/bar/sub/foo.bar",
+            },
+        )
