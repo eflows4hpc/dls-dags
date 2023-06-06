3cf1a097fecca2cbac5797f49cc06b313440c7c1
jj <j.rybicki@fz-juelich.de>
2023-06-06 11:17:16 +0200

    change in default params

--- tests/test_webdavs.py
+++ tests/test_webdavs.py
@@ -118,7 +118,7 @@ class TestWebDAV(unittest.TestCase):
     def test_resolve(self, hook):
 
         ret = resolve_oid(oid=777)
-        self.assertEqual(ret, ("default_webdav", "dls"))
+        self.assertEqual(ret, ("default_webdav", "dls/"))
 
         mm = MagicMock()
         mm.get_entry.return_value = json.dumps(
