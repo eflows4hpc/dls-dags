075db20ab8c55005e08f8aecc5ddc7db509dc89b
jj <j.rybicki@fz-juelich.de>
2024-02-05 15:26:46 +0100

    touch for writing

--- mlflow_stagein.py
+++ mlflow_stagein.py
@@ -47,6 +47,7 @@ def model_stagein():
             
 
         ssh_hook = get_connection(conn_id=connection_id, **context)
+        clt = ssh_hook.get_conn()
         sftp_client = ssh_hook.get_conn().open_sftp()
 
         with open(ret, "rb") as sr:
@@ -55,7 +56,7 @@ def model_stagein():
             if file_exist(sftp=sftp_client, name=target_name):
                 print(target_name," exists. Overwritting.")
 
-            sftp_client.exec_command(command=f"touch {target_name}")
+            clt.exec_command(command=f"touch {target_name}")
             with sftp_client.open(target_name, "wb") as tr:
                 tr.set_pipelined(pipelined=True)
                 copy_streams(inp=sr, outp=tr)
